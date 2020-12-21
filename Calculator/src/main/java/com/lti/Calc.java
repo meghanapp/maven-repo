28ab3ebcd17dcf903d2e48d007e7a3926b4aea70
megha <meghanapallempati@gmail.com>
2020-12-21 14:59:27 +0530

    second commit

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -4,7 +4,12 @@ public class Calc {
      public static int add(int num1,int num2) {
     	 return num1+num2;
      }
+     public static int subs(int num1,int num2) {
+		// TODO Auto-generated method stub
+         return num1-num2;
+	}
      public static void main(String[] args) {
 		System.out.println(add(10,20));
+		System.out.println(subs(10,20));
 	}
 }
