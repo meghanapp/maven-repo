cf9753438e52c64a8b1adeb64638c5a41cfabd52
meghanapp <meghanapallempati@gmail.com>
2020-12-21 15:10:28 +0530

    commit with new changes

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -8,8 +8,12 @@ public class Calc {
 		// TODO Auto-generated method stub
          return num1-num2;
 	}
+     public static int prod(int num1,int num2) {
+    	 return num1*num2;
+     }
      public static void main(String[] args) {
 		System.out.println(add(10,20));
 		System.out.println(subs(10,20));
+		System.out.println(prod(10,20));
 	}
 }
